2ee649b118fa020da1ceffd405bf9b47fa7caba2
mjroney <50534698+mjroney@users.noreply.github.com>
2019-08-06 23:00:51 -0400

    Create date_decorator.py

---
+++ date_decorator.py
@@ -0,0 +1,20 @@
+from datetime import date, time, datetime
+
+def datetimer(func):
+    def function_wrapper(X):
+        print('Enter your ', X, 'in \n YYYY, MM, DD format.')
+        func(X)
+        date_entry = input(': ')
+	    year, month, day = map(int, date_entry.split(', '))
+	    return date(year, month, day)
+    return function_wrapper
+
+
+@datetimer
+def date_name(X):
+	pass
+
+
+
+# Call like:
+# date_name('birthday')
