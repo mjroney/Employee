8371043be0636d041932e4b9354804718797de7e
mjroney <50534698+mjroney@users.noreply.github.com>
2019-08-06 22:58:44 -0400

    Create vacations.py

---
+++ vacations.py
@@ -0,0 +1,48 @@
+class vacation():
+
+    holidays(2020) = {
+    'New Years Day' : datetime.date(2020, 1, 01),
+    'Martin Luther King Day' : datetime.date(2020, 1, 20),
+    'Good Friday' : datetime.date(2020, 4, 10),
+    'Memorial Day' : datetime.date(2020, 5, 25),
+    'Independance Day' : datetime.date(2020, 7, 04),
+    'Labor Day' : datetime.date(2020, 9, 07),
+    'Thanksgiving Day' : datetime.date(2020, 11, 26),
+    'Black Friday' : datetime.date(2020, 11, 27),
+    'Christmas Eve Day' : datetime.date(2020, 12, 24),
+    'Christmas Day' : datetime.date(2020, 12, 25),
+    }
+    
+    def __init__(self, amount):
+        self.amount = amount
+
+    def date_checker():
+        print(holidays)
+        prompt = input('Are the dates correct (y/n)? \n')
+        if prompt = 'y':
+            pass
+        elif prompt = 'n':
+            for items in holidays:
+                key = input('What is the new date for {}?', key)
+                    date_checker()
+
+    for items in vacation_days:
+        if service_length <= key:
+            amount = value
+        elif service_length > timedelta(days = 365.25 * 24):
+            amount = 25
+
+
+    vacation_days = {
+        timedelta(days = 365.25): 0,
+        timedelta(days = 365.25 * 2): 5,
+        timedelta(days = 365.25 * 8): 10,
+        timedelta(days = 365.25 * 15): 15,
+        timedelta(days = 365.25 * 24): 20,
+        }
+
+    def days_at_new_year():
+        service_length = benefit_date - datetime.date(2020, 01, 01)
+
+    def days_end_of_year():
+        service_length = benefit_date - datetime.date(2020, 12, 31)
