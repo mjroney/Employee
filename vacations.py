95502911bc05255f927b7017ddd01d96939f02bd
mjroney <50534698+mjroney@users.noreply.github.com>
2019-09-10 15:46:26 -0400

    Update vacations.py
    putting in some of my notes. Not ready for use.

--- vacations.py
+++ vacations.py
@@ -1,20 +1,35 @@
 class vacation():
 
-    holidays(2020) = {
-    'New Years Day' : datetime.date(2020, 1, 01),
-    'Martin Luther King Day' : datetime.date(2020, 1, 20),
-    'Good Friday' : datetime.date(2020, 4, 10),
-    'Memorial Day' : datetime.date(2020, 5, 25),
-    'Independance Day' : datetime.date(2020, 7, 04),
-    'Labor Day' : datetime.date(2020, 9, 07),
-    'Thanksgiving Day' : datetime.date(2020, 11, 26),
-    'Black Friday' : datetime.date(2020, 11, 27),
-    'Christmas Eve Day' : datetime.date(2020, 12, 24),
-    'Christmas Day' : datetime.date(2020, 12, 25),
-    }
-    
-    def __init__(self, amount):
-        self.amount = amount
+    def __init__(self, single_day, v_week, total_days, holiday)
+        self.single_day = single_day()
+        self.v_week = v_week()
+        self.total_days = total_days()
+        self.holiday = holiday
+
+    holiday = {{2020:
+        ('New Years Day' : datetime.date(2020, 1, 01),
+        'Martin Luther King Day' : datetime.date(2020, 1, 20),
+        'Good Friday' : datetime.date(2020, 4, 10),
+        'Memorial Day' : datetime.date(2020, 5, 25),
+        'Independance Day' : datetime.date(2020, 7, 04),
+        'Labor Day' : datetime.date(2020, 9, 07),
+        'Thanksgiving Day' : datetime.date(2020, 11, 26),
+        'Black Friday' : datetime.date(2020, 11, 27),
+        'Christmas Eve Day' : datetime.date(2020, 12, 24),
+        'Christmas Day' : datetime.date(2020, 12, 25),)},
+        {2021:
+        ('New Years Day' : datetime.date(2021, 1, 01),
+        'Martin Luther King Day' : datetime.date(2021, 1, 20)
+        'Good Friday' : datetime.date(2021, 4, 10),
+        'Memorial Day' : datetime.date(2021, 5, 25),
+        'Independance Day' : datetime.date(2021, 7, 04),
+        'Labor Day' : datetime.date(2021, 9, 07),
+        'Thanksgiving Day' : datetime.date(2021, 11, 26),
+        'Black Friday' : datetime.date(2021, 11, 27),
+        'Christmas Eve Day' : datetime.date(2021, 12, 24),
+        'Christmas Day' : datetime.date(2021, 12, 25),)
+        }
+
 
     def date_checker():
         print(holidays)
@@ -32,6 +47,37 @@ class vacation():
         elif service_length > timedelta(days = 365.25 * 24):
             amount = 25
 
+def vacation_days():
+    benefit_date = input(': ')
+    print('Benefit date is: \n', benefit)
+    today = date.today()
+    print("Today's date is : \n", today)
+    new_year = date((today.year +1), 1, 1)
+    print('The next new year is: \n', new_year)
+    end_of_year = date((today.year +2), 1, 1)
+    print('End of year is calculated to: \n', end_of_year)
+
+    milestones = {}
+    milestones.update([
+    (first_yr = benefit_date.replace(benefit_date.year + 1)),
+    (five_days = benefit_date.replace(benefit_date.year + 2)),
+    (ten_days = benefit_date.replace(benefit_date.year + 8)),
+    (fifteen_days = benefit_date.replace(benefit_date.year + 15)),
+    (twenty_days = benefit_date.replace(benefit_date.year + 24)),
+    ])
+    for item in milestones:
+        print(item)
+    
+    future_vacations = {
+    one: benefit_date.replace(x.year + 1),
+    two: benefit_date.replace(x.year + 2),
+    three: benefit_date.replace(x.year + 8),
+    four: benefit_date.replace(x.year + 15),
+    five: benefit_date.replace(x.year + 24),
+    }
+    for value in future_vacations.values():
+        if value < today:
+            amount += 1
 
     vacation_days = {
         timedelta(days = 365.25): 0,
